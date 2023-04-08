1ba92334ed43f0b3f6a5c9f72c960dd7f61af142
boyscout99 <tommaso.praturlon@gmail.com>
2023-04-08 09:25:16 +0200

    correct IP address to connecto Prometheus API

--- src/agent/promQL.py
+++ src/agent/promQL.py
@@ -1,7 +1,7 @@
 from prometheus_api_client import PrometheusConnect
 import requests.exceptions
 
-prom = PrometheusConnect(url='http://localhost:9090')
+prom = PrometheusConnect(url='http://prometheus.istio-system.svc.cluster.local:9090')
 
 query = 'istio_request_duration_milliseconds_count{app="frontend", response_code="200",response_flags="-",request_protocol="http",source_app="loadgenerator"}'
 try:
