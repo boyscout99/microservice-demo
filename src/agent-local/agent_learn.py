9a33810f116449dfa4b38e5a64e02776b8416d0e
boyscout99 <tommaso.praturlon@gmail.com>
2023-04-17 10:32:03 +0200

    updated queries

--- src/agent-local/agent_learn.py
+++ src/agent-local/agent_learn.py
@@ -17,13 +17,12 @@ if not os.path.exists(logs_dir):
 # Define the hyperparameters for training
 alpha = 100  # Your desired value for alpha
 # QUERIES FOR FRONTEND DEPLOYMENT
-# request duration [ms]
-q_request_duration = 'rate(istio_request_duration_milliseconds_count{app="frontend", response_code="200",response_flags="-",request_protocol="http",source_app="loadgenerator"}[1m])'
+# take slowest service request duration [ms]
+q_request_duration = 'max(rate(istio_request_duration_milliseconds_count{pod=~"frontend-.*", request_protocol="grpc", response_code="200"}[1m]))'
 # cpu utilisation [%]
-q_cpu_usage = 'rate(container_cpu_usage_seconds_total{pod=~"frontend-.*"}[1m])/0.2'
-# memory usage [Mb]
-# !TODO change the image name
-q_memory_usage = 'rate(container_memory_usage_bytes{pod=~"frontend-.*"}[1m])/1000000'
+q_cpu_usage = 'rate(container_cpu_usage_seconds_total{pod=~"frontend-.*"}[1m])/0.1*100'
+# memory usage [%]
+q_memory_usage = 'rate(container_memory_usage_bytes{pod=~"frontend-.*"}[1m])/64000000'
 # number of replicas per deployment
 q_pod_replicas = 'count(kube_pod_info{pod=~"frontend-.*"})'
 
