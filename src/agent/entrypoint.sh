44dd11651c31b39ef098dc13e28d64e809511d26
boyscout99 <tommaso.praturlon@gmail.com>
2023-04-21 15:09:53 +0200

    give up kubecheck, 50g storage, mount path

--- src/agent/entrypoint.sh
+++ src/agent/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the health_check Flask app in the backgound
-python3 health_check.py &
+python3 app/health_check.py &
 
 # Run the agent in foreground
-python3 agent_learn.py
+python3 app/agent_learn.py
