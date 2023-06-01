eb320c6221cee7c8f0ebb85bd736db12b592c945
boyscout99 <tommaso.praturlon@gmail.com>
2023-06-01 16:59:42 +0200

    script for experiment

---
+++ dummy/scale_replicas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+deployment="productcatalogservice"
+namespaces=("rl-agent-e1-a2c" "testing" "rl-agent-e3-1")
+
+replicas=1
+while [ $replicas -le 4 ]; do
+    for namespace in "${namespaces[@]}"; do
+        echo "Scaling deployment $deployment in namespace $namespace to $replicas replicas"
+        kubectl scale deployment -n $namespace $deployment --replicas $replicas
+    done
+    replicas=$((replicas + 1))
+    sleep 30  # Sleep for 10 minutes
+done
