d366419275a8360eefdf648cfec87c677ef272df
boyscout99 <tommaso.praturlon@gmail.com>
2023-06-02 15:33:54 +0200

    multiple metrics for plot

--- dummy/grafana_data.py
+++ dummy/grafana_data.py
@@ -11,30 +11,62 @@ df.fillna(0, inplace=True)
 
 # Extract timestamps and signals from DataFrame
 timestamps = pd.to_datetime(df['Time'])
-rps1 = df['RPS-testing']
-rps2 = df['RPS-rl-agent-e1-a2c']
-rps3 = df['RPS-rl-agent-e1-ppo']
-
-rps = pd.DataFrame({'rps1': rps1, 'rps2': rps2, 'rps3': rps3})
+# Filter columns that start with 'RPS'
+rps_columns = [col for col in df.columns if col.startswith('RPS')]
+cpu_columns = [col for col in df.columns if col.startswith('CPU')]
+rep_columns = [col for col in df.columns if col.startswith('replicas')]
+# Create a new DataFrame with selected columns
+rps = df[rps_columns]
+cpu = df[cpu_columns]
+rep = df[rep_columns]
 
 # the time length of the timeseries is len(df)*5s
 total_time_sec = len(timestamps)*5
 print(f"total_timestamps: {len(timestamps)}, total_time_sec: {total_time_sec}")
 # 2.5 minutes correspond to 2*60+30s = 150s = 30 index
 rgb = 0
+fig, axs = plt.subplots(3, 1)
+axs[0].set_title("RPS")
+axs[1].set_title("CPU")
+axs[2].set_title("replicas")
+
 for col in rps:
     rgb += 0.1
-    plt.plot(timestamps, rps[col].values, color=(0.5+rgb, 0.5+rgb, 0.5+rgb), label=f"{col}")
+    axs[0].plot(timestamps, rps[col].values, color='b', label=f"{col}")
 
+for col in cpu:
+    rgb += 0.1
+    axs[1].plot(timestamps, cpu[col].values, color='b', label=f"{col}")
+
+for col in rep:
+    rgb += 0.1
+    axs[1].plot(timestamps, rep[col].values, color='b', label=f"{col}")
+
+# Create a dictionary for the measurements
+measurements = []
+"""
+[
+    { // this is interval 1
+        "rps":[1(mean), 2(standard deviation), 3(standard error)],
+        "cpu: [4(mean), 5(std), 6(stde)]
+    },
+    { // this is interval 2
+        ...
+    }
+    
+]
+"""
 
 # Create a rolling window with a size of 2.5 minutes and a step of 2.5 minutes
 # Resample the DataFrame to 2.5 minute intervals
 i = 60
 j = 25
-means = []
 size = len(rps)
-
+interval = 0
+metrics_df = [rps, cpu, rep]
+metrics_df_order = ["rps", "cpu", "rep"]
 for index in range(0, size, i):
+    interval += 1
     left = index+j
     right = index+2*j
     if left>=size: break
@@ -44,11 +76,29 @@ for index in range(0, size, i):
         right = right
     print(f"left margin: {left}, right margin: {right}")
     rgb = 0
-    for col in rps:
-        rgb += 0.01
-        window = rps[col].iloc[left:right].values
-        plt.plot(timestamps.iloc[left:right], window, color=(rgb, rgb, rgb))
-        mean = np.mean(window)
+    # for every measurement compute the mean of the window
+    metric_idx = 0
+    for df in metrics_df:
+        measure = {}
+        means = []
+        for col in df:
+            rgb += 0.01
+            window = df[col].iloc[left:right].values
+            axs[metric_idx].plot(timestamps.iloc[left:right], window, color='r')
+            mean = np.mean(window)
+            # plot the mean as horizontal line
+            x_min = timestamps.iloc[index + j]
+            x_max = timestamps.iloc[right - 1]
+            axs[metric_idx].hlines(y=mean, xmin=x_min, xmax=x_max, color='c', linestyle='--')
+            means.append(mean)
+        # compute standard deviation and standard error for the interval
+        std = np.std(means)
+        stde = np.std(means, ddof=1) / np.sqrt(np.size(means))
+        measure[metrics_df_order[metric_idx]] = [np.mean(means), std, stde]
+        metric_idx += 1
+        measurements.append(measure)
+        print(f"measurements:\n{measurements}")
+
 
     # window_1 = signal1.iloc[index+j:right]
     # window_2 = signal2.iloc[index+j:right]
