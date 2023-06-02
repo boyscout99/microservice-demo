d03d512ccbb5c9b20267d4f37850497e45a239b8
boyscout99 <tommaso.praturlon@gmail.com>
2023-06-02 21:46:01 +0200

    moved deploy loadgen

--- dummy/scale_replicas.sh
+++ dummy/scale_replicas.sh
@@ -4,11 +4,11 @@ deployment="productcatalogservice"
 namespaces=("rl-agent-e1-a2c" "testing" "rl-agent-e1-ppo" "rl-agent-e3-1" "rl-agent-e3-2" "rl-agent-e4-1")
 
 replicas=1
-while [ $replicas -le 30 ]; do
+while [ $replicas -le 15 ]; do
     for namespace in "${namespaces[@]}"; do
         echo "Scaling deployment $deployment in namespace $namespace to $replicas replicas"
         kubectl scale deployment -n $namespace $deployment --replicas $replicas
     done
     replicas=$((replicas + 1))
-    sleep 600  # Sleep for 10 minutes
+    sleep 400  # Sleep for 8 minutes
 done
