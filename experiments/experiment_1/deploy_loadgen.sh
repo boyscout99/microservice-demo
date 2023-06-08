895ecee11269357fe49c940f87fc593340d5e1b7
boyscout99 <tommaso.praturlon@gmail.com>
2023-06-08 15:47:51 +0200

    ready experiment 2 first part

--- experiments/experiment_1/deploy_loadgen.sh
+++ experiments/experiment_1/deploy_loadgen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 namespaces=("default" "testing" "rl-agent-e1-a2c" "rl-agent-e1-ppo" "rl-agent-e3-1" "rl-agent-e3-2" "rl-agent-e4-1")
-auxiliary=("frontend" "recommendationservice" "checkoutservice")
+auxiliary=("frontend" "recommendationservice" "checkoutservice" "currencyservice" "adservice")
 
 for namespace in "${namespaces[@]}"; do
     for service in "${auxiliary[@]}"; do
         echo "Scaling up bottleneck service $service to 20 replicas in namespace $namespace"
-        kubectl scale deployment -n $namespace $service --replicas 20
+        kubectl scale deployment -n $namespace $service --replicas 5
     done
     echo "Deploying loadgenerator in namespace $namespace"
     kubectl apply -f /mnt/nfs-client/microservice-demo/kubernetes-manifests/$namespace/loadgenerator.yaml -n $namespace
