4ac09adcef956abfbbd6c4957c8835091cce0435
boyscout99 <tommaso.praturlon@gmail.com>
2023-06-14 16:01:06 +0200

    fix logs

--- src/agent/experiment_2_2.py
+++ src/agent/experiment_2_2.py
@@ -1,5 +1,42 @@
-from Scale import KubernetesEnvironment
+# For experiment 2.2, I need something that updates replicas 
+# on all namespaces from within the cluster.
 import time
+from datetime import datetime, timedelta
+import logging
+import os
+import sys
+from Scale import KubernetesEnvironment
+from Logger import LoggerWriter
+
+NAMESPACE = "rl-agent-e5-a2c"
+MODEL = "A2C"
+
+t = datetime.now()
+t = t + timedelta(hours=2) # UTC+2
+timestamp = t.strftime("%Y_%m_%d_%H%M%S")
+# Get the absolute path of the script directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+pod_logs_dir = os.path.join(script_dir, f"pod_logs/{NAMESPACE}/{MODEL}")
+
+def enable_logging(pod_logs_dir):
+    pod_log_file = os.path.join(pod_logs_dir, f"{MODEL}_learn_{timestamp}.log")
+    # logging.basicConfig(filename=pod_log_file, level=logging.DEBUG)  # Initialize logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format=f"{timestamp} [%(levelname)s] %(message)s",
+        filename=pod_log_file
+    )
+    
+    # Define a logger
+    my_logger = logging.getLogger(__name__)
+
+    # Redirect console output to logger
+    sys.stdout = LoggerWriter(my_logger.info)
+    sys.stderr = LoggerWriter(my_logger.error)
+
+    return my_logger
+
+logger = enable_logging(pod_logs_dir)
 
 deployment="productcatalogservice"
 namespaces=[
