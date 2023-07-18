5949b41808c953f4e35832fa1df3eac3c0c32433
boyscout99 <tommaso.praturlon@gmail.com>
2023-07-18 11:16:40 +0200

    working training automation

--- experiments/experiment_2/train_subprocess.py
+++ experiments/experiment_2/train_subprocess.py
@@ -1,14 +1,25 @@
 import subprocess as sub
+import shutil 
 import os
+
+def delete_files(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if file_path.startswith(os.path.join(folder_path, "best")):
+                continue  # Skip files in best
+            print(f"Removing file: {file_path}")
+            # os.remove(file_path)
+            shutil.rmtree(file_path)
 
 # Get the absolute path of the script directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
-print(f"script_dir: {script_dir}")
+# print(f"script_dir: {script_dir}")
 models_path = os.path.join(script_dir, 'models/rl-agent-2/A2C/')
 tf_logs_path = os.path.join(script_dir, 'tf_logs/rl-agent-2/A2C/')
-print(f"models_path: {models_path}, tf_logs_path: {tf_logs_path}")
+# print(f"models_path: {models_path}, tf_logs_path: {tf_logs_path}")
 folders = sorted(os.listdir(models_path))
-print(f"folders: {folders}")
+# print(f"folders: {folders}")
 
 S2=["CPU", "p95", "mem"]
 S3=["CPU", "p95"]
@@ -22,7 +33,7 @@ states = [S2]
 for state in states:
     print(f"State {state}")
     # Perform 5 runs for each state
-    for i in range(0,2):
+    for i in range(0,1):
         c_agent_learn = ["echo",
             "python3", "agent_learn.py",
             "--deployment", "productcatalogservice",
@@ -42,10 +53,10 @@ for state in states:
         # Pick best episode:
         # Take last model in last folder 2023_07_...
         folders = sorted(os.listdir(models_path))
-        last_folder = os.path.join(models_path, folders[-1])
-        print(f"Last folder: {last_folder}")
-        existing_models = [f for f in os.listdir(last_folder)]
-        print(f"existing_models: {existing_models}")
+        last_model_folder = os.path.join(models_path, folders[-1])
+        print(f"Last model folder: {last_model_folder}")
+        existing_models = [f for f in os.listdir(last_model_folder)]
+        # print(f"existing_models: {existing_models}")
         try:
             # Sort models by their names to get the last saved model
             existing_models.sort(key=lambda  x: int(x.split('.')[0]))
@@ -55,35 +66,23 @@ for state in states:
         except Exception as e:
             print(f"ERROR in picking best model - {e}")
         # Delete all other models
-        # Go to models directory and create directory best
-        c_mkdir = [
-            # "echo", 
-            "mkdir", f"{last_folder}/best"
-        ]
-        # Move best model to best/ and remove all other models
-        c_rm_models = [
-            # "echo",
-            "mv", f"{last_saved_model}", f"{last_folder}/best",
-            "rm", "-rf", f"{last_folder}/*.zip"
-        ]
-        sub.run(c_mkdir)
-        sub.run(c_rm_models)
-        # Go to directory tf_logs/
-        c_cd_last_tf_dir = [
-            # "echo",
-            "cd", f"{tf_logs_path}"
-        ]
-        sub.run(c_cd_last_tf_dir)
-        # Go to last directory
-        sub.run(c_cd_last_dir)
-        # Create directory best
-        sub.run(c_mkdir)
-        # Delete all logs except the best one
-        best_tf_log = "A2C_"+best_model_number
-        c_rm_tf_logs = [
-            # "echo",
-            "mv", f"{best_tf_log}", "best",
-            "rm", "-rf", "A2C_*"
-        ]
-        sub.run(c_rm_tf_logs)
+        for model in existing_models:
+            file_path = last_model_folder + '/' + model
+            if file_path != last_model_folder + '/' + last_saved_model:
+                print(f"Remove: {file_path}")
+                os.remove(file_path)
+
+        # Delete all tf_logs/ except best
+        folders = sorted(os.listdir(tf_logs_path))
+        last_tf_folder = os.path.join(tf_logs_path, folders[-1])
+        print(f"Last tf folder: {last_tf_folder}")
+
+        best_tf_log = "A2C_" + best_model_number
+        for log in os.listdir(last_tf_folder):
+            file_path = last_tf_folder + '/' + log
+            if file_path != last_tf_folder + '/' + best_tf_log:
+                print(f"Remove: {file_path}")
+                shutil.rmtree(file_path)
+                # os.rmdir(file_path)
+
 
